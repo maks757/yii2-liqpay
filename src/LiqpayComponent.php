e28c47510e1d82178e67817df3def83a2ee0f5d6
maks757 <maks757q@gmail.com>
2017-06-15 01:01:52 +0300

    init

---
+++ src/LiqpayComponent.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cherednyk Maxim
+ * Email: maks757q@gmail.com
+ * Phone: +380639960375
+ * Date: 15.06.2017
+ * Time: 0:39
+ */
+
+namespace maks757\liqpay\sec;
+
+
+class LiqpayComponent
+{
+
+}
